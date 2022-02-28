c9300746ac56ef2a0c3da1028cb7e8ee90ffb9de
Nancy Chisenga <nanciechisenga@gmail.com>
2022-02-28 17:09:35 +0000

    learning about json and updating the bookcase application

--- session04/src/components/BookCounter.js
+++ session04/src/components/BookCounter.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import '../BookCounter.css';
 
 const BookCounter = (props) => {
 
   return (
     <main>
       <h2>{props.library.name}'s Books</h2>
-      {props.library.books.map((book) => (<div>{book.title}</div>))}
+      {props.library.books.map((book) => (
+      <li >
+        <h3 className='booklist'>{book.volumeInfo.title}</h3>
+        <img src={book.volumeInfo.imageLinks.smallThumbnail} alt={book.volumeInfo.title}/>
+        <p className='description'>Description: {book.volumeInfo.description}</p> 
+        <ul>
+        {book.volumeInfo.authors.map((author) => {
+          <li>{author}</li>
+        })}
+        </ul>
+        <p className='price'>Price: £{book.saleInfo.retailPrice && book.saleInfo.retailPrice.amount}</p>
+        <hr></hr>
+        </li>))}
+   <button  onClick={() =>
+    props.refreshBooks()}>Refresh
+    Books</button>
     </main>
   );
 }
